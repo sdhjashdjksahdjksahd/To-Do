c6b8a49c09c8a2d36609bfd2932266e3845a452e
sdhjashdjksahdjksahd <audreydon0516@gmail.com>
2024-06-27 21:52:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); }); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
